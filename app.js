55a6bf4ad76e35b8eb76fe8868586bf974b2172b
mairomuuga <mairo.muuga@khk.ee>
2021-09-20 11:25:43 +0300

    Variables

---
+++ app.js
@@ -0,0 +1,34 @@
+// Muutuja
+// var, let, const
+// Deklareerimine
+
+//let name = "paks beps" ;
+//console.log(name);
+//name = "Skinny Nef" ;
+//console.log(name);
+
+// Lubatud margid nime loomisel - tahed, numbrid, _, $.
+//numbriga muutuja nimi ei tohi!
+
+//let $ = "dolts" ;
+//console.log($);
+
+//const name = "nef" ;
+//console.log(name);
+// Const ei saa muuta vaartust
+//name = "Chris";
+
+// Massiivid
+const Numbers = [1, 2, 3, 4, 5];
+Numbers.push(6);
+console.log(Numbers);
+
+// Objektid
+const person ={
+	name: "Kadi" ,
+	age: 32
+};
+
+person.name = "Kati" ,
+person.age = 13
+console.log(person);
