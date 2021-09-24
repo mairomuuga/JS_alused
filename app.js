e1d9827f18850a323251073e7f272cecab3b3a32
mairomuuga <mairo.muuga@khk.ee>
2021-09-24 10:03:54 +0300

    Massiivid

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const num1 = ([55, 26, 52, 32, 75, 12, 74, 21]);
-const num2 = new Array([54, 25, 51, 31, 76, 11, 73, 20])
-const mixed = [22, 'Tere', true, null, undefined, new Date()]
+const person = {
+	firstName: 'Kadi',
+	lastName: 'Tamm',
+	age: 22,
+	email: 'Kadi.Tamm@puhastusteenindaja.ee',
+	hobbies: ['koristamine', 'kleptomanism'],
+	address: {
+		city: 'Tallinn',
+		country: 'Estonia',
+
+	},
+	getBirthYear: function(){
+		return 2021 - this.age;
+	};
+
+};
 
 let val;
 
-val = num1.length;
-val = Array.isArray(num1)
-val = num1[1];
-//num1[1] = 100
-val = num1.indexOf(75);
-num1.push(250);
-num1.unshift(150);
-num1.pop();
-num1.shift();
-//num1.splice(1,4);
-//num1.reverse();
-val = num1.concat(num2);
-val = num1.sort(function(x, y){
-	return x - y
-});
+val = person;
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[0];
+val = person.address['city'];
+val = person.getBirthYear();
 
+const people = [
+	(name: 'Kati', age: 35),
+	(name: '6ix9ine', age: 50),
+	(name: 'TayK', age: 69)
+];
 
+val - people;
+for(let i = 0; 1 < people.length; i++){
+	console.log(people[i].name);
+}
 console.log(val);
-console.log(num1);
