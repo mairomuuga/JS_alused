fec4cee78267bee9c3bc7cb40194686accea925c
mairomuuga <mairo.muuga@khk.ee>
2021-09-27 10:54:45 +0300

    Objektid/Ajatootlus

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
-const person = {
-	firstName: 'Kadi',
-	lastName: 'Tamm',
-	age: 22,
-	email: 'Kadi.Tamm@puhastusteenindaja.ee',
-	hobbies: ['koristamine', 'kleptomanism'],
-	address: {
-		city: 'Tallinn',
-		country: 'Estonia',
-
-	},
-	getBirthYear: function(){
-		return 2021 - this.age;
-	}
-};
-
 let val;
 
-val = person;
-val = person.firstName;
-val = person['lastName'];
-val = person.age;
-val = person.hobbies[0];
-val = person.address['city'];
-val = person.getBirthYear();
+const today = new Date();
 
-const people = [
-	{name: 'Kati', age: 35},
-	{name: '6ix9ine', age: 50},
-	{name: 'TayK', age: 69}
-];
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
 
-val = people;
-for(let i = 0; i < people.length; i++){
-	console.log(people[i].name);
-}
-console.log(val);
+let birthday = new Date('11/9/2001')
+
+console.log(today);
+console.log(birthday);
