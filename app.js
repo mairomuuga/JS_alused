00cdb18331c32dcb83b4a684dae14dbdaacf18aa
mairomuuga <mairo.muuga@khk.ee>
2021-09-29 15:35:11 +0300

    element ja atribuut

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-let val;
-
-const list=document.querySelector('ul');
-const listItem=document.querySelector('li:first-child');
-
-val=list;
-val=listItem;
-
-val=list.childNodes;
-val=list.childNodes[0].nodeName;
-val=list.childNodes[0].nodeType;
-val=list.children;
-val=list.children[1];
-list.children[1].textContent = 'Study XML';
-val=list.children[2].children;
-list.children[2].children[0].id="test-id";
-val=list.firstChild;
-val=list.firstElementChild;
-val=list.lastChild;
-val=list.lastElementChild;
-val=list.childElementCount;
-val=listItem.parentNode;
-val=listItem.parentElement;
-val=listItem.parentElement.parentElement;
-val=listItem.nextSibling;
-val=listItem.nextElementSibling.nextElementSibling.previousElementSibling;
-
-
-console.log(val);
+//elemendi loomine
+const li =document.createElement('li');
+const list=document.querySelector("ul");
+//klassi m22ramine
+li.className="collection-item";
+li.appendChild(document.createTextNode("P22sta mati vangist!"));
+const link = document.createElement("a");
+link.className="secondary-content";
+link.appendChild(document.createTextNode("X"));
+//lisa atribuut v22rtusega
+link.setAttribute("href", "#");
+li.appendChild(link);
+//elemendi lisamine teise elemendi sisse
+list.appendChild(li);
+	
+console.log(li);
